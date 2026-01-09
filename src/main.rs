a7abaae62e47e5de7ef48ba8744e537f823c4414
Arunjot-ATR592 <arunjot@antiersolutions.com>
2026-01-09 16:47:13 +0530

    chore: Added code for password hasing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 pub mod utils;
+use utils::{hash_password, verify_password };
 
 fn main() {
-    println!("Hello, world!");
+    let hash = hash_password("my_password").unwrap();
+    println!("HASH = {}", hash);
+
+    let valid = verify_password("my_password", &hash).unwrap();
+    println!("VALID = {}", valid);
 }
