25c13da3ba090be1382e3cc1e3477830d48f2da9
Arunjot-ATR592 <arunjot@antiersolutions.com>
2026-01-12 16:13:43 +0530

    chore: Added code for db-connection

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 pub mod utils;
 pub mod models;
-use utils::{hash_password, verify_password };
+use std::env;
+pub use dotenvy::dotenv;
 
- async fn signup(){
+use diesel::{ Connection, PgConnection};
+use utils::{ hash_password, verify_password };
 
-    }
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+    let data_base_url = env::var("DATABASE_URL").expect("no env file found");
+    PgConnection::establish(&data_base_url).unwrap_or_else(|_| panic!("Nothing Found"))
+}
+
+async fn signup() {}
 
 fn main() {
-
+    establish_connection();
     let hash = hash_password("my_password").unwrap();
     println!("HASH = {}", hash);
 
     let valid = verify_password("my_password", &hash).unwrap();
     println!("VALID = {}", valid);
-
 }
